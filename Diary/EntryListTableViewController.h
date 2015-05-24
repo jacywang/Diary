2812bb5f539fe71ac19cdd002c69d0c3345025f2
Jacy Wang <jacywj@gmail.com>
2015-05-24 11:03:57 -0700

    Add EntryListTableViewController

---
+++ Diary/EntryListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryListTableViewController.h
+//  Diary
+//
+//  Created by JIAN WANG on 5/24/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryListTableViewController : UITableViewController
+
+@end
