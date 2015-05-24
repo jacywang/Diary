40d91409579b94ef848ddeb60e450ff74ca03243
Jacy Wang <jacywj@gmail.com>
2015-05-23 22:03:40 -0700

    Create NSManagedObject subclass

---
+++ Diary/DiaryEntry.h
@@ -0,0 +1,27 @@
+//
+//  DiaryEntry.h
+//  Diary
+//
+//  Created by JIAN WANG on 5/23/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ENUM(int16_t, DiaryEntryMood) {
+    DiaryEntryMoodGood = 0,
+    DiaryEntryMoodAverage = 1,
+    DiaryEntryMoodBad = 2
+    
+};
+
+@interface DiaryEntry : NSManagedObject
+
+@property (nonatomic, retain) NSString * body;
+@property (nonatomic, retain) NSString * location;
+@property (nonatomic) NSTimeInterval date;
+@property (nonatomic) int16_t mood;
+@property (nonatomic, retain) NSData * imageData;
+
+@end
---
+++ Diary/DiaryEntry.m
@@ -0,0 +1,20 @@
+//
+//  DiaryEntry.m
+//  Diary
+//
+//  Created by JIAN WANG on 5/23/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import "DiaryEntry.h"
+
+
+@implementation DiaryEntry
+
+@dynamic body;
+@dynamic location;
+@dynamic date;
+@dynamic mood;
+@dynamic imageData;
+
+@end
